d0e28878b1559e7243268d9f218e5345e08a763b
shota-nukumizu <author2000.1225@gmail.com>
2022-01-09 06:57:41 +0900

    create component for the menu

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    <Navbar />
+
+    <section class="section">
+      <router-view/>
+    </section>
   </div>
-  <router-view/>
 </template>
+
+<script>
+import Navbar from '@/components/layout/Navbar'
+export default {
+  name: 'App',
+  components: {
+    Navbar
+  }
+}
+</script>
 
 <style lang="scss">
 @import '../node_modules/bulma';
---
+++ src/components/layout/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+    <nav class="navbar is-dark">
+        <div class="navbar-brand">
+            <router-link to="/" class="navbar-item">
+                <strong>Vue CRM Project</strong>
+            </router-link>
+        </div>
+
+        <div class="navbar-menu">
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="buttons">
+                        <router-link to="/sign-up" class="button is-success"><strong>Sign Up</strong></router-link>
+                        <router-link to="/log-in" class="button is-light"><strong>Log in</strong></router-link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'Navbar'
+}
+</script>
